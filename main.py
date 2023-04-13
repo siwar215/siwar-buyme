15a2b09072f7da0c8dc6f2113b118aff99cbfc48
siwarkhateeb <siwarkhateeb215@gmail.com>
2023-04-13 14:18:12 +0300

    Go to link function

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import json
+from lib2to3.pgen2 import driver
 from unittest import TestCase
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 from pages.base_page import BasePage
+from pages.registration_screen import Registration
+from unittest import TestCase
 
 
 class TestRegistration(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        print("setup")
-        f = open('config.json', 'r')  # TODO: move to env ?
+    def setUp(self):
+        f = open('config.json', 'r')
         config_json = json.load(f)
-        cls.cfg = config_json
-        driver_path = cls.cfg['drivers']['chrome']
-        cls.driver = webdriver.Chrome(service=Service(driver_path))
-        cls.base_page = BasePage(cls.driver)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
+        self.cfg = config_json
+        driver_path = self.cfg['drivers']['chrome']
+        self.driver = webdriver.Chrome(service=Service(driver_path))
+        self.base_page = BasePage(self.driver)
 
     def test_a_success_register(self):
-        self.base_page.run_test()
-        # base.goto_link(driver, base_page.datajson['urls']['buymehome'])
-        # self.home_page.click_on_login()
-        # self.home_page.click_on_register()
-        # self.home_page.verify_title_Registration()
-        # self.register_page.register_success()
+        BasePage.goto_link(self.base_page, self.cfg['url']['buymehomepage'])
+
+    # @classmethod
+    # def setUpClass(cls):
+    #
+    #     f = open('/Users/siwarkhateeb/Downloads/siwar-buyme/config.json', 'r')
+    #     config_json = json.load(f)
+    #     cls.cfg = config_json
+    #     driver_path = cls.cfg['drivers']['chrome']
+    #     cls.driver = webdriver.Chrome(service=Service(driver_path))
+    #     cls.base_page = BasePage(cls.driver)
+    #
+    # @classmethod
+    # def tearDownClass(cls):
+    #     cls.driver.quit()
--- pages/registration_screen.py
+++ pages/registration_screen.py
@@ -1,16 +1,25 @@
 from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
 
 
-class RegistrationPage(BasePage):
+class Constants:
+    register_first_name = By.XPATH, "//input[@placeholder='שם פרטי']"
+    register_email = By.XPATH, "//input[@placeholder='מייל']"
+    register_password = By.ID, "valPass"
+    register_password_conf = By.XPATH, "//input[@placeholder='אימות סיסמה']"
+    agree_radio = By.XPATH, "//span[@class='circle']"
+    submit = By.XPATH, "//button[@type='submit']"
+    email_required_error = By.ID, "parsley-id-27"  # "//input[@data-parsley-id='23']" #By.CLASS_NAME, "parsley-required"
+    password_mismach_error = By.CLASS_NAME, "parsley-equalto"
 
+
+class Registration(BasePage):
     def __init__(self, driver):
         BasePage.__init__(self, driver)
-        self.driver.get('https://buyme.co.il')
 
-    def click_register_area(self):
-        self.click_element(By.LINK_TEXT, "כניסה / הרשמה")
-
-    def wait_for_registration_form(self):
-        self.wait_for_element(By.XPATH, "//span[text()='thisisatest']")
+    def register_success(self):
+        BasePage.wait_and_enter_text(self, Constants.register_first_name, "register_success")
+        BasePage.wait_and_enter_text(self, Constants.register_email, "test@email.com")
+        BasePage.wait_and_enter_text(self, Constants.register_password, "Password")
+        BasePage.wait_and_enter_text(self, Constants.register_password_conf, "Password2")
+        BasePage.wait_and_click_on_element(self, Constants.agree_radio)
