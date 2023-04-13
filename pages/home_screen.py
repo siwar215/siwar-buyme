3c63c250804ca59942867ba3b4360943169f023b
siwarkhateeb <siwarkhateeb215@gmail.com>
2023-04-13 23:11:48 +0300

    Starting with the home screen

---
+++ pages/home_screen.py
@@ -0,0 +1,42 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+from pages.base_page import BasePage
+
+
+class Constants:
+    # login_register = By.LINK_TEXT, "כניסה / הרשמה"
+    login_register = By.CLASS_NAME, "notSigned"
+    login_email = By.XPATH, '//input[@type="email"]' # TODO: Verify why not inserted
+    login_pass = By.XPATH, '// *[ @ id = "ember2225"]'
+    # login_email = By.CSS_SELECTOR, "h1[class=bm-h1]"
+    # login_password = By.ID, "ember2225"
+    submit = By.XPATH, "//button[@type='submit']"
+
+
+
+
+class Home(BasePage):
+    def __init__(self, driver):
+        BasePage.__init__(self, driver)
+
+    def setUp(self):
+        f = open('config.json', 'r')
+        config_json = json.load(f)
+        self.cfg = config_json
+        driver_path = self.cfg['drivers']['chrome']
+        self.driver = webdriver.Chrome(service=Service(driver_path))
+        self.base_page = BasePage(self.driver)
+
+    # Once logged in by inserting a valid credentials
+    def click_on_login_button(self):
+        BasePage.wait_and_click_on_element(self, Constants.login_register)
+
+    def login_valid_credentials(self):
+        BasePage.wait_and_enter_text(self, Constants.login_email, "siwarkhateeb215@gmail.com")
+        BasePage.wait_and_enter_text(self, Constants.login_pass, "Password123")
+        BasePage.wait_and_click_on_element(self, Constants.submit)
+
+
---
+++ tests/main_test_home.py
@@ -0,0 +1,25 @@
+import json
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from pages.home_screen import Home
+from unittest import TestCase
+
+
+class TestRegistration(TestCase):
+
+    def setUp(self):
+        f = open('../config.json', 'r')
+        config_json = json.load(f)
+        self.cfg = config_json
+        driver_path = self.cfg['drivers']['chrome']
+        self.driver = webdriver.Chrome(service=Service(driver_path))
+        self.home_screen = Home(self.driver)
+
+    def test_Once_logged_in(self):
+        self.home_screen.goto_link(self.cfg['url']['buymehomepage'])
+        self.home_screen.click_on_login_button()
+        self.home_screen.login_valid_credentials()
+        time.sleep(10)
